4c06bce9178a7c256203f77480ef474ae79893ee
Logan Esch <lesch@live.com>
2015-06-13 17:32:41 +0000

    added skeleton for application.

---
+++ application/app.py
@@ -0,0 +1,14 @@
+"""
+ python application to talk to ardunio.
+ logs data from arduino and displays it to GUI.
+"""
+COM_PORT = "/dev/ttyACM0"
+
+#establish connection with arduino
+
+#read data
+#manipulate data
+#store data
+#display data
+
+
