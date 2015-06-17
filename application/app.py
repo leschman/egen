6d1ba6029de6bade49015dcdb7324c6ae482e8b5
Logan Esch <lesch@live.com>
2015-06-17 12:53:42 -0600

    Write results and time to csv file. Fixes #7

--- application/app.py
+++ application/app.py
@@ -3,8 +3,10 @@
  logs data from arduino and displays it to GUI.
 """
 from serial import Serial
+from datetime import datetime
+import csv
 
-
+RESULTS_FILE = "./application/results.csv"
 COM_PORT = "/dev/ttyACM0"
 BAUD_RATE = 9600
 TIMEOUT = 1
@@ -15,21 +17,26 @@ ser = Serial(COM_PORT, BAUD_RATE, timeout=TIMEOUT)
 while ser.read() == 'A':
 	ser.write("a")
 
-
-#read data
-while ser.isOpen():
-	readings = []
-	for i in range(0,9):
-		voltage = ser.readline()
-		#clean up input.
-		voltage = voltage[:voltage.index('\r')]
-		voltage = ( int(voltage, 2)* .004882812)
-		readings.append(voltage)
-	average = sum(readings)/len(readings)
-	data.append(average)
-	print voltage
-	print len(data)	
-#	print repr(voltage)
+#set up csv results file.
+with open(RESULTS_FILE, 'wb') as csvfile:
+	writer = csv.writer(csvfile)
+	writer.writerows([['Time','Voltage']])
+	while ser.isOpen():
+		readings = []
+		#read data
+		for i in range(0,9):
+			voltage = ser.readline()
+			#clean up input.
+			voltage = voltage[:voltage.index('\r')]
+			voltage = ( int(voltage, 2)* .004882812)
+			readings.append(voltage)
+		average = sum(readings)/len(readings)
+		time = datetime.now().time()
+		row = [[time.strftime('%H%M%S%f'),average]]
+		writer.writerows(row)
+		data.append(average)
+		print voltage
+		print len(data)	
 
 #manipulate data
 #store data
