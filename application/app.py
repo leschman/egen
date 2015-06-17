73d06b395d1d4a99c0694b190f5cd522887e614d
Logan Esch <lesch@live.com>
2015-06-17 14:12:15 -0600

    added real time GUI plot and value display.

--- application/app.py
+++ application/app.py
@@ -5,13 +5,16 @@
 from serial import Serial
 from datetime import datetime
 import csv
+import numpy as np
+import matplotlib.pyplot as plt
 
 RESULTS_FILE = "./application/results.csv"
 COM_PORT = "/dev/ttyACM0"
 BAUD_RATE = 9600
 TIMEOUT = 1
-data = []
-
+GRAPH_WIDTH = 100
+SAMPLE_SIZE = 40
+count = 0
 #establish connection with arduino
 ser = Serial(COM_PORT, BAUD_RATE, timeout=TIMEOUT)
 while ser.read() == 'A':
@@ -21,10 +24,18 @@ while ser.read() == 'A':
 with open(RESULTS_FILE, 'wb') as csvfile:
 	writer = csv.writer(csvfile)
 	writer.writerows([['Time','Voltage']])
+
+	#set up real time ploting
+	fig = plt.figure()
+	plt.axis([0,GRAPH_WIDTH,0,6])
+	plt.title(' volts')
+	plt.ion()
+	plt.show()
+
+	#read data
 	while ser.isOpen():
 		readings = []
-		#read data
-		for i in range(0,9):
+		for i in range(0,SAMPLE_SIZE):
 			voltage = ser.readline()
 			#clean up input.
 			voltage = voltage[:voltage.index('\r')]
@@ -34,10 +45,25 @@ with open(RESULTS_FILE, 'wb') as csvfile:
 		time = datetime.now().time()
 		row = [[time.strftime('%H%M%S%f'),average]]
 		writer.writerows(row)
-		data.append(average)
 		print voltage
-		print len(data)	
+		plt.scatter(count, average)
+		plt.title('{0} volts'.format(average))
+		plt.draw()
+		count = count + 1
+		if count == GRAPH_WIDTH:
+			plt.clf()
+			plt.axis([0,GRAPH_WIDTH,0,6])
+			plt.title('volts')
+			plt.ion()
+			plt.show()
+			count = 0
+	print len(data)	
 
+
+
+def update_line(num, data, line):
+	line.set_data(data[...,:num])
+	return line
 #manipulate data
 #store data
 #display data
