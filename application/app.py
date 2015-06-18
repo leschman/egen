be6793487defc20f24899b20dd7bf94c9174a7c7
Logan Esch <lesch@live.com>
2015-06-18 12:56:26 -0600

    refactor to add stability.

--- application/app.py
+++ application/app.py
@@ -14,7 +14,31 @@ BAUD_RATE = 9600
 TIMEOUT = 1
 GRAPH_WIDTH = 100
 SAMPLE_SIZE = 100
+VOLTAGE_STEP_SIZE = .004882812
 count = 0
+
+def update_line(num, data, line):
+	line.set_data(data[...,:num])
+	return line
+
+#manipulate data
+def manipulate_data(data):
+	try:
+		data = data[:data.index('\r')]
+		data = (int(data, 2) * VOLTAGE_STEP_SIZE)
+		return float(data)
+	except ValueError:
+		return None
+
+#set up real time ploting
+def setup_plot():
+	plt.clf()
+	plt.axis([0,GRAPH_WIDTH,0,6])
+	plt.title(' volts')
+	plt.ion()
+	plt.show()
+	count = 0
+
 #establish connection with arduino
 ser = Serial(COM_PORT, BAUD_RATE, timeout=TIMEOUT)
 while ser.read() == 'A':
@@ -25,22 +49,20 @@ with open(RESULTS_FILE, 'wb') as csvfile:
 	writer = csv.writer(csvfile)
 	writer.writerows([['Time','Voltage']])
 
-	#set up real time ploting
 	fig = plt.figure()
-	plt.axis([0,GRAPH_WIDTH,0,6])
-	plt.title(' volts')
-	plt.ion()
-	plt.show()
+	setup_plot()
 
 	#read data
 	while ser.isOpen():
 		readings = []
 		for i in range(0,SAMPLE_SIZE):
 			voltage = ser.readline()
+		
 			#clean up input.
-			voltage = voltage[:voltage.index('\r')]
-			voltage = ( int(voltage, 2)* .004882812)
-			readings.append(voltage)
+			data = manipulate_data(voltage)
+			if data:
+				readings.append(data)
+
 		average = sum(readings)/len(readings)
 		time = datetime.now().time()
 		row = [[time.strftime('%H%M%S%f'),average]]
@@ -51,21 +73,7 @@ with open(RESULTS_FILE, 'wb') as csvfile:
 		plt.draw()
 		count = count + 1
 		if count == GRAPH_WIDTH:
-			plt.clf()
-			plt.axis([0,GRAPH_WIDTH,0,6])
-			plt.title('volts')
-			plt.ion()
-			plt.show()
-			count = 0
+			setup_plot()
 	print len(data)	
-
-
-
-def update_line(num, data, line):
-	line.set_data(data[...,:num])
-	return line
-#manipulate data
-#store data
-#display data
 
 
